2f26ff148d0496e999cfb996520b19a750e24243
Brett Bloxom <brettbloxom@gmail.com>
2019-11-03 15:47:55 -0700

    delete comment mutation

--- src/index.js
+++ src/index.js
@@ -93,6 +93,7 @@ const typeDefs = `
       createPost(data: CreatePostInput): Post! 
       deletePost(id: ID!): Post!
       createComment(data: CreateCommentInput!): Comment! 
+      deleteComment(id: ID!): Comment!
     }
 
     input CreateUserInput {
@@ -263,7 +264,19 @@ const resolvers = {
       comments.push(comment)
       return comment
     },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(comment => comment.id === args.id)
+
+      if (commentIndex === -1) {
+        throw new Error('Comment not found')
+      }
+
+      const deletedComments = comments.splice(commentIndex, 1)
+
+      return deletedComments[0]
+    },
   },
+
   Post: {
     author(parent, args, ctx, info) {
       return users.find(user => user.id === parent.author)
