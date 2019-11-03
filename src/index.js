793121327b1f10456d6fa515715f07fe1ddee010
Brett Bloxom <brettbloxom@gmail.com>
2019-11-03 15:34:46 -0700

    delete post mutation

--- src/index.js
+++ src/index.js
@@ -91,6 +91,7 @@ const typeDefs = `
       createUser(data: CreateUserInput): User! 
       deleteUser(id: ID!): User!
       createPost(data: CreatePostInput): Post! 
+      deletePost(id: ID!): Post!
       createComment(data: CreateCommentInput!): Comment! 
     }
 
@@ -221,6 +222,28 @@ const resolvers = {
 
       posts.push(post)
       return post
+    },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex(post => post.id === args.id)
+
+      if (postIndex === -1) {
+        throw new Error('Post not found')
+      }
+
+      const deletedPosts = posts.splice(postIndex, 1)
+
+      posts = posts.filter(post => {
+        const match = post.id === args.id
+
+        if (match) {
+          comments = comments.filter(comment => comment.post !== post.id)
+        }
+
+        return !match
+      })
+      comments = comments.filter(comment => comment.post !== args.id)
+
+      return deletedPosts[0]
     },
     createComment(parent, args, ctx, info) {
       const userExists = users.some(user => user.id === args.data.author)
