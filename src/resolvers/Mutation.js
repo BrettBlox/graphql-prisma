296eb36c1c8c2bfdaa47857fcee23ffd1859f55e
Brett Bloxom <brettbloxom@gmail.com>
2019-11-06 20:22:04 -0700

    created comments subscription. subscriptions are fucking cool

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -119,7 +119,7 @@ const Mutation = {
 
     return post
   },
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author)
     if (!userExists) {
       throw new Error('User not found')
@@ -135,6 +135,8 @@ const Mutation = {
     }
 
     db.comments.push(comment)
+    pubsub.publish(`comment ${args.data.post}`, { comment })
+
     return comment
   },
   deleteComment(parent, args, { db }, info) {
