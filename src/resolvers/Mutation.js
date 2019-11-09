5c0fa48870b1c5ab9025610ad7501c1ff2217970
Brett Bloxom <brettbloxom@gmail.com>
2019-11-08 19:45:14 -0700

    mutation subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,19 +1,20 @@
-import uuid from 'uuidv4'
+import uuidv4 from 'uuid/v4'
 
 const Mutation = {
   createUser(parent, args, { db }, info) {
     const emailTaken = db.users.some(user => user.email === args.data.email)
 
     if (emailTaken) {
-      throw new Error('Email taken.')
+      throw new Error('Email taken')
     }
 
     const user = {
-      id: uuid(),
+      id: uuidv4(),
       ...args.data,
     }
 
     db.users.push(user)
+
     return user
   },
   deleteUser(parent, args, { db }, info) {
@@ -25,16 +26,16 @@ const Mutation = {
 
     const deletedUsers = db.users.splice(userIndex, 1)
 
-    posts = db.posts.filter(post => {
+    db.posts = db.posts.filter(post => {
       const match = post.author === args.id
 
       if (match) {
-        comments = db.comments.filter(comment => comment.post !== post.id)
+        db.comments = db.comments.filter(comment => comment.post !== post.id)
       }
 
       return !match
     })
-    comments = db.comments.filter(comment => comment.author !== args.id)
+    db.comments = db.comments.filter(comment => comment.author !== args.id)
 
     return deletedUsers[0]
   },
@@ -45,17 +46,22 @@ const Mutation = {
     if (!user) {
       throw new Error('User not found')
     }
+
     if (typeof data.email === 'string') {
       const emailTaken = db.users.some(user => user.email === data.email)
+
       if (emailTaken) {
         throw new Error('Email taken')
       }
+
       user.email = data.email
     }
+
     if (typeof data.name === 'string') {
       user.name = data.name
     }
-    if (typeof data.email !== 'undefined') {
+
+    if (typeof data.age !== 'undefined') {
       user.age = data.age
     }
 
@@ -63,12 +69,13 @@ const Mutation = {
   },
   createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author)
+
     if (!userExists) {
       throw new Error('User not found')
     }
 
     const post = {
-      id: uuid(),
+      id: uuidv4(),
       ...args.data,
     }
 
@@ -82,6 +89,7 @@ const Mutation = {
         },
       })
     }
+
     return post
   },
   deletePost(parent, args, { db, pubsub }, info) {
@@ -133,16 +141,20 @@ const Mutation = {
             data: originalPost,
           },
         })
-      } else if (!originalPost && post.published) {
+      } else if (!originalPost.published && post.published) {
         pubsub.publish('post', {
-          post: 'CREATED',
-          data: post,
+          post: {
+            mutation: 'CREATED',
+            data: post,
+          },
         })
       }
     } else if (post.published) {
       pubsub.publish('post', {
-        post: 'UPDATED',
-        data: post,
+        post: {
+          mutation: 'UPDATED',
+          data: post,
+        },
       })
     }
 
@@ -150,16 +162,16 @@ const Mutation = {
   },
   createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author)
-    if (!userExists) {
-      throw new Error('User not found')
-    }
-    const postExists = db.posts.some(post => post.id === args.data.post)
-    if (!postExists) {
-      throw new Error('Post not found')
+    const postExists = db.posts.some(
+      post => post.id === args.data.post && post.published
+    )
+
+    if (!userExists || !postExists) {
+      throw new Error('Unable to find user and post')
     }
 
     const comment = {
-      id: uuid(),
+      id: uuidv4(),
       ...args.data,
     }
 
@@ -186,7 +198,7 @@ const Mutation = {
     const comment = db.comments.find(comment => comment.id === id)
 
     if (!comment) {
-      throw new Error('comment not found')
+      throw new Error('Comment not found')
     }
 
     if (typeof data.text === 'string') {
@@ -197,4 +209,4 @@ const Mutation = {
   },
 }
 
-export default Mutation
+export { Mutation as default }
