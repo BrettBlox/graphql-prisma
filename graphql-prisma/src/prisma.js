aecfbfd58f43cc73bbe01b0c088e292714828274
Brett Bloxom <brettbloxom@gmail.com>
2019-11-25 18:51:37 -0700

    using prisma bindings

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -2,5 +2,13 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'localhost:4466',
+  endpoint: 'http://localhost:4466',
+})
+
+prisma.query.users(null, '{ id name email }').then(data => {
+  console.log(JSON.stringify(data, undefined, 4))
+})
+
+prisma.query.comments(null, '{ id text author { id name } }').then(data => {
+  console.log(JSON.stringify(data, undefined, 4))
 })
